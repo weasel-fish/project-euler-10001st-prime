8704ea1eb6b2882994150eb93b06ee26b6cd3945
github username <github email address>
2021-09-30 12:06:15 -0400

    Done

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,25 @@
 # Implement your procedural solution here!
+def prime_number_for(nth)
+    prime_count = 0
+    number = 0
+
+    while prime_count < nth
+        number += 1
+        prime_count += 1 if prime?(number)
+    end
+
+    number
+end
+
+def prime?(num)
+    if num < 2
+        return false
+    end
+
+    (2..Math.sqrt(num).to_i).each do |n|
+        if num % n == 0
+            return false
+        end
+    end
+    true
+end
